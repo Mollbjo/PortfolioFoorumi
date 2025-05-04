00df3566ae421f63e2e610c7482d149964a6c39e
Mollbjo <joonamollberg@gmail.com>
2025-05-04 21:50:35 +0300

    Fix CSRF token problems

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.secret_key=config.secret_key
 
 def check_csrf():
-    if "csfr_token" not in request.form:
+    if "csrf_token" not in request.form:
         abort(403)
     if request.form["csrf_token"] != session["csrf_token"]:
         abort(403)
@@ -79,7 +79,7 @@ def login():
         if user_id:
             session["user_id"] = user_id
             session["username"] = username
-            session["csfr_token"] = secrets.token_hex(16)
+            session["csrf_token"] = secrets.token_hex(16)
             return redirect("/")
         else:
             flash ("VIRHE: Väärä käyttäjätunnus tai salasana", "error")
